e9a555973d2f65ffcdd225fb921de5fc39b6c708
gingercook <vahitaras@gmail.com>
2021-09-18 00:05:35 -0400

    Except a small bug, database is inserted for this project

--- server.js
+++ server.js
@@ -4,12 +4,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config();
 
 // console.log(process.env.email);
 
 // Load the full build.
 const _ = require('lodash');
+
+const main = async () => {
+  await mongoose.connect('mongodb://localhost:27017/blogDB');
+};
+
+main().catch(err => {
+  console.log(err);
+});
+
+const blogPostSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+// Pass third parameter if you dont want your collections to be plurilized by MongoDB
+const blogPost = mongoose.model('blogpost', blogPostSchema, 'blogpost');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -23,6 +40,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let postArray = [];
+
+blogPost.find((err, allPostsObj) => {
+  if(!err) {
+    console.log(allPostsObj);
+
+    allPostsObj.forEach(postObj => {
+      const post = {
+        title: postObj.title,
+        content: postObj.content
+      }
+
+      postArray.push(post);
+    });
+
+  } else {
+    console.error(err);
+  }
+});
 
 app.get('/', function(req, res){
   res.render(__dirname + "/views/home.ejs", {homeStartingContent: homeStartingContent, postArray: postArray});
@@ -41,12 +76,43 @@ app.get("/compose", function(req, res) {
 });
 
 app.post('/', function(req, res) {
-  let post = {
+  let newPost = new blogPost({
     title: req.body.postTitle,
     content: req.body.postBody
+  });
+
+  const findData = () => {
+    return new Promise((res, rej) => {
+      blogPost.find((err, allPostsObj) => {
+        if(!err) {
+          console.log(allPostsObj);
+          res(allPostsObj);
+
+          postArray = [];
+
+          allPostsObj.forEach(postObj => {
+            const post = {
+              title: postObj.title,
+              content: postObj.content
+            }
+
+            postArray.push(post);
+          });
+
+        } else {
+          console.error(err);
+          rej([]);
+        }
+      });
+    });
   };
 
-  postArray.push(post);
+  const dbSaveShow = async () => {
+    await newPost.save();
+    await findData();
+  };
+
+  dbSaveShow();
 
   res.redirect('/');
 
